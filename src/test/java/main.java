0447b83f1315b8da55d5654524edf665b108509d
DANG Truong <dangtruong080593@gmail.com>
2021-02-12 09:48:04 +0700

    experiment with TestNG

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,63 +1,84 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
 public class main {
 
-    public static void main(String[] args) {
+    private Car myToyotaCar = new Car();
+    private Car myBmwCar = new Car();
 
-        // Question 1
-        System.out.println("Question 1's output");
-        System.out.println(Constant.LINE_BREAK);
-        Car myToyotaCar = new Car();
+    @BeforeClass
+    public void setUp() {
+
         myToyotaCar.setSeats(Constant.SEATS)
                 .setBrand(Constant.TOYOTA)
                 .setWheels(Constant.WHEELS)
                 .setDoors(Constant.DOORS)
                 .setMaxSpeed(100);
-        myToyotaCar.info();
 
-        Car myBmwCar = new Car();
         myBmwCar.setSeats(Constant.SEATS)
                 .setBrand(Constant.BMW)
                 .setWheels(Constant.WHEELS)
                 .setDoors(Constant.DOORS)
                 .setMaxSpeed(200);
+    }
+
+    @Test(priority = 0)
+    public void questionOne() {
+        System.out.println("Question 1's output");
+        System.out.println(Constant.LINE_BREAK);
+
+        myToyotaCar.info();
         myBmwCar.info();
 
         System.out.println(Constant.LINE_BREAK);
 
         myToyotaCar.run();
         myBmwCar.run();
+    }
 
-        // Question 2
-//        System.out.println(Constant.LINE_BREAK);
-//        System.out.println("Question 2's output");
-//        System.out.println(Constant.LINE_BREAK);
-//        Thread toyotaThread = new Thread(myToyotaCar);
-//        toyotaThread.start();
-//        Thread bmwThread = new Thread(myBmwCar);
-//        bmwThread.start();
+    @Test(priority = 1)
+    public void questionTwo() throws InterruptedException {
+        System.out.println(Constant.LINE_BREAK);
+        System.out.println("Question 2's output");
+        System.out.println(Constant.LINE_BREAK);
 
-        // Question 4
+        Thread toyotaThread = new Thread(myToyotaCar);
+        Thread bmwThread = new Thread(myBmwCar);
+
+        toyotaThread.start();
+        Thread.sleep(10);
+        bmwThread.start();
+    }
+
+    @Test(priority = 2)
+    public void questionFour() throws InterruptedException {
         System.out.println(Constant.LINE_BREAK);
         System.out.println("Question 4's output");
         System.out.println(Constant.LINE_BREAK);
+
         ConcurrentCar myNewToyotaCar = new ConcurrentCar();
         ConcurrentCar myNewBmwCar = new ConcurrentCar();
+
         myNewToyotaCar.setSeats(Constant.SEATS)
                 .setBrand(Constant.TOYOTA)
                 .setWheels(Constant.WHEELS)
                 .setDoors(Constant.DOORS)
                 .setMaxSpeed(100);
-        myNewToyotaCar.info();
+
         myNewBmwCar.setSeats(Constant.SEATS)
                 .setBrand(Constant.BMW)
                 .setWheels(Constant.WHEELS)
                 .setDoors(Constant.DOORS)
                 .setMaxSpeed(200);
+
+        myNewToyotaCar.info();
         myNewBmwCar.info();
 
         Thread toyotaConcurrentThread = new Thread(myNewToyotaCar);
-        toyotaConcurrentThread.start();
         Thread bmwConcurrentThread = new Thread(myNewBmwCar);
-        bmwConcurrentThread.start();
 
+        toyotaConcurrentThread.start();
+        bmwConcurrentThread.start();
     }
+
 }
